867d4f191b4f80303308d68771db6126cfc1e449
Md. Jakaria Ibna Azam Khan <mdjakariaibnaazamkhan@gmail.com>
2024-07-24 21:53:42 +0600

    all medicine list page design complete.

---
+++ lib/data/medicine data.dart
@@ -0,0 +1,32 @@
+class MedicineData{
+  final String Medicine_Name;
+  final int Quantity;
+  MedicineData({required this.Medicine_Name, required this.Quantity});
+}
+
+List<MedicineData> demoMedicineData = [
+  MedicineData(Medicine_Name: "Setra", Quantity: 500),
+  MedicineData(Medicine_Name: 'Coral Max D', Quantity: 200),
+  MedicineData(Medicine_Name: 'Pantonix 20', Quantity: 800),
+  MedicineData(Medicine_Name: 'Amit', Quantity: 200),
+  MedicineData(Medicine_Name: 'Pregaben 75', Quantity: 100),
+  MedicineData(Medicine_Name: 'Nexito 10', Quantity: 300),
+  MedicineData(Medicine_Name: "Setra", Quantity: 500),
+  MedicineData(Medicine_Name: 'Coral Max D', Quantity: 200),
+  MedicineData(Medicine_Name: 'Pantonix 20', Quantity: 800),
+  MedicineData(Medicine_Name: 'Amit', Quantity: 200),
+  MedicineData(Medicine_Name: 'Pregaben 75', Quantity: 100),
+  MedicineData(Medicine_Name: 'Nexito 10', Quantity: 300),
+  MedicineData(Medicine_Name: "Setra", Quantity: 500),
+  MedicineData(Medicine_Name: 'Coral Max D', Quantity: 200),
+  MedicineData(Medicine_Name: 'Pantonix 20', Quantity: 800),
+  MedicineData(Medicine_Name: 'Amit', Quantity: 200),
+  MedicineData(Medicine_Name: 'Pregaben 75', Quantity: 100),
+  MedicineData(Medicine_Name: 'Nexito 10', Quantity: 300),
+  MedicineData(Medicine_Name: "Setra", Quantity: 500),
+  MedicineData(Medicine_Name: 'Coral Max D', Quantity: 200),
+  MedicineData(Medicine_Name: 'Pantonix 20', Quantity: 800),
+  MedicineData(Medicine_Name: 'Amit', Quantity: 200),
+  MedicineData(Medicine_Name: 'Pregaben 75', Quantity: 100),
+  MedicineData(Medicine_Name: 'Nexito 10', Quantity: 300),
+];
--- lib/screen/Admin/show_all_medicine_page.dart
+++ lib/screen/Admin/show_all_medicine_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:just_medical_center_automation/data/medicine%20data.dart';
 
 class ShowAllMedicinePage extends StatelessWidget {
   const ShowAllMedicinePage({super.key});
@@ -6,9 +7,88 @@ class ShowAllMedicinePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('All Medicine show page'),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30,vertical: 10),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+                  Center(
+        child: Text('All Medicine List',style: Theme.of(context).textTheme.headlineLarge!.copyWith(fontWeight: FontWeight.bold),),
+
+                  ),
+            SizedBox(height: 30,),
+            Expanded(
+              child: ListView.builder(
+                itemCount: demoMedicineData.length+1,
+               itemBuilder: (context, index) {
+                return Table(
+                   textDirection: TextDirection.ltr,
+                   border: TableBorder.all(color: Colors.black, width: 1.0),
+                   children: [
+                   index==0?  TableRow(
+                         children:[
+                           Heading(text: 'SL No',),
+                           Heading(text: 'Medicine Name'),
+                           Heading(text: 'Quantity')
+                         ]
+                     ):
+                     TableRow(
+                       children: [
+                         Data(text: (index).toString()),
+                        Data(text: demoMedicineData[index-1].Medicine_Name) ,
+                         Data(text: demoMedicineData[index-1].Quantity.toString())
+                       ]
+                     )
+                     ,
+
+
+                   ],
+                 );
+               },
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
 }
+
+class Heading extends StatelessWidget {
+  final String text;
+  const Heading({
+    super.key, required this.text,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Text(text,textAlign: TextAlign.center,style: TextStyle(
+        fontSize: 22,
+        fontWeight: FontWeight.w700
+      ),),
+    );
+  }
+}
+
+class Data extends StatelessWidget {
+  final String text;
+  const Data({
+    super.key, required this.text,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Text(text,textAlign: TextAlign.center,style: TextStyle(
+          fontSize: 18,
+          fontWeight: FontWeight.w400
+      ),),
+    );
+  }
+}
+
+
