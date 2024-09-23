b15c5767f5de309ecc60d1eb2d9a7373f676604d
Md. Jakaria Ibna Azam Khan. <mdjakariaibnaazamkhan@gmail.com>
2024-09-23 22:28:01 +0600

    doctor dashboard design complete without responsive.

--- lib/screen/Doctor/doctor_screen.dart
+++ lib/screen/Doctor/doctor_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:just_medical_center_automation/widget/doctor/doctor_sidebar.dart';
 
+import '../../widget/doctor/dashboardMiddlePart.dart';
+
 class DoctorScreen extends StatefulWidget {
   const DoctorScreen({super.key});
 
@@ -20,67 +22,25 @@ class _DoctorScreenState extends State<DoctorScreen> {
           ),
           Expanded(
               flex: 7,
-              child: Container(
-                color: Colors.grey.withOpacity(0.4),
-
-                child: Stack(
-
-                  children: [
-                    Opacity(
-                      opacity: 0.3,
-                      child: Image.asset('images/doctor searching.jpg',fit: BoxFit.cover,width:MediaQuery.of(context).size.width ,
-                        height: MediaQuery.of(context).size.height),
-                    ),
-                    Positioned(
-                      top: 0,right: 0,
-                      child: Stack(
-                        children: [
-                          Container(
-                            margin: EdgeInsets.only(right: 20,top: 20),
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(20),
-                              color: Colors.black.withOpacity(0.3)
-                            ),
-                            child: SizedBox(
-                              width: MediaQuery.of(context).size.width/4,
-                              child: TextField(
-
-                                decoration: InputDecoration(
-                                  hintText: 'Search for a patient by ID',
-                                  hintStyle: TextStyle(fontWeight: FontWeight.w300,
-                                  color: Colors.white),
-                                  border: InputBorder.none
-                                ),
-                              ),
-                            ),
-                          ),
-                          Positioned(top: 0,right: 0,bottom: 0,child: Container(
-                            margin: EdgeInsets.only(top: 20),
-                            width: 80,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(20),
-                              color: Theme.of(context).colorScheme.primary
-                            ),
-                            child: Icon(
-                              Icons.search,
-                              color: Colors.white,
-                            ),
-                          ))
-                        ],
-                      ),
-                    ),
-
-                  ],
-                ),
-              )
+              child: DashBoardMiddlePart()
           ),
           Expanded(
               flex: 2,
               child: Container(
                 color: Colors.white,
+                child: Column(
+                  children: [
+                    Padding(padding: EdgeInsets.only(top: 50)),
+                    Text('Patient History',textAlign: TextAlign.center,style: TextStyle(fontSize: 20,
+                    fontWeight: FontWeight.w600,letterSpacing: 2,wordSpacing: 5),),
+                    Divider()
+                  ],
+                ),
               )),
         ],
       ),
     );
   }
 }
+
+
---
+++ lib/widget/doctor/dashboardMiddlePart.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+class DashBoardMiddlePart extends StatelessWidget {
+  const DashBoardMiddlePart({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.grey.withOpacity(0.4),
+
+      child: Stack(
+
+        children: [
+          Opacity(
+            opacity: 0.3,
+            child: Image.asset('images/doctor searching.jpg',fit: BoxFit.cover,width:MediaQuery.of(context).size.width ,
+                height: MediaQuery.of(context).size.height),
+          ),
+          Positioned(
+            top: 0,right: 0,
+            child: Stack(
+              children: [
+                Container(
+                  margin: EdgeInsets.only(right: 20,top: 20),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.black.withOpacity(0.3)
+                  ),
+                  child: SizedBox(
+                    width: MediaQuery.of(context).size.width/4,
+                    child: TextField(
+
+                      decoration: InputDecoration(
+                          hintText: 'Search for a patient by ID',
+                          hintStyle: TextStyle(fontWeight: FontWeight.w300,
+                              color: Colors.white),
+                          border: InputBorder.none
+                      ),
+                    ),
+                  ),
+                ),
+                Positioned(top: 0,right: 0,bottom: 0,child: InkWell(
+                  onTap: (){
+
+                  },
+                  child: Container(
+                    margin: EdgeInsets.only(top: 20),
+                    width: 80,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Theme.of(context).colorScheme.primary
+                    ),
+                    child: Icon(
+                      Icons.search,
+                      color: Colors.white,
+                    ),
+                  ),
+                ))
+              ],
+            ),
+          ),
+
+        ],
+      ),
+    );
+  }
+}
