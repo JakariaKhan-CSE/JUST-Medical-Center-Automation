d55e3ea4c49689a3de9a0cead4e9000eff46f9ba
Md. Jakaria Ibna Azam Khan. <mdjakariaibnaazamkhan@gmail.com>
2024-10-01 17:54:43 +0600

    added secure function in doctor controller.

--- lib/controller/doctorController/doctor_controller.dart
+++ lib/controller/doctorController/doctor_controller.dart
@@ -2,6 +2,22 @@ import 'package:flutter/cupertino.dart';
 
 class DoctorController extends ChangeNotifier{
   int current_index = 0;
+  bool _oldPasswordSecure = true;
+  bool _newPasswordSecure = true;
+  bool _confirmPasswordSecure = true;
+
+  bool get oldPasswordSecure => _oldPasswordSecure;
+  void  setOldPasswordSecure(){
+    _oldPasswordSecure = !_oldPasswordSecure;
+  }
+  bool get newPasswordSecure => _newPasswordSecure;
+  void  setNewPasswordSecure(){
+    _newPasswordSecure = !_newPasswordSecure;
+  }
+  bool get confirmPasswordSecure => _confirmPasswordSecure;
+  void  setConfirmPasswordSecure(){
+    _confirmPasswordSecure = !_confirmPasswordSecure;
+  }
 
   void SetIndex(index)
   {
