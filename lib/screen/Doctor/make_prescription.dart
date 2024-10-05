049324901adae8d45d8fcb32798706c0d3e3116d
Md. Jakaria Ibna Azam Khan. <mdjakariaibnaazamkhan@gmail.com>
2024-10-05 18:31:16 +0600

    few amount code refactor.

--- lib/screen/Doctor/make_prescription.dart
+++ lib/screen/Doctor/make_prescription.dart
@@ -14,139 +14,168 @@ class MakePrescription extends StatefulWidget {
 class _MakePrescriptionState extends State<MakePrescription> {
   @override
   Widget build(BuildContext context) {
-    final _controller = Provider.of<DoctorController>(context,listen: true);
+    final _controller = Provider.of<DoctorController>(context, listen: true);
     return Scaffold(
-backgroundColor: Colors.tealAccent.withOpacity(0.3),
+      backgroundColor: Colors.tealAccent.withOpacity(0.3),
       body: Consumer<DoctorController>(
-          builder: (context, doctorNotifier, child) {
-            return Column(
-              children: [
-                Expanded(
-                  child: ListView.builder(
-                    padding: EdgeInsets.symmetric(horizontal: 30,vertical: 20),
-                    itemCount: doctorNotifier.textEditingControllerList.length,
-                    shrinkWrap: true,
-                    itemBuilder: (context, index) {
-                      final textController = doctorNotifier.textEditingControllerList[index];
-                      final daysController = doctorNotifier.daysTextEditingControllerList[index];
+        builder: (context, doctorNotifier, child) {
+          return Column(
+            children: [
+              Expanded(
+                child: ListView.builder(
+                  padding: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+                  itemCount: doctorNotifier.textEditingControllerList.length,
+                  shrinkWrap: true,
+                  itemBuilder: (context, index) {
+                    final textController =
+                        doctorNotifier.textEditingControllerList[index];
+                    final daysController =
+                        doctorNotifier.daysTextEditingControllerList[index];
 
-                      // for checkbox
-                      final morning = doctorNotifier.morningList[index];
-                      final afternoon = doctorNotifier.afternoonList[index];
-                      final night = doctorNotifier.nightList[index];
-                      final beforeMeal = doctorNotifier.beforeList[index];
+                    // for checkbox
+                    final morning = doctorNotifier.morningList[index];
+                    final afternoon = doctorNotifier.afternoonList[index];
+                    final night = doctorNotifier.nightList[index];
+                    final beforeMeal = doctorNotifier.beforeList[index];
                     return Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 8,horizontal: 5),
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 8, horizontal: 5),
                       child: Column(
                         children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              CustomTextField(
-                                  fieldController: textController,
-                                  label: "Medicine Name",
-                                  textInputType: TextInputType.text),
-
-                              Container(
-                                width: MediaQuery.of(context).size.width*0.08,
-                                decoration: BoxDecoration(
-                                  color: Colors.white,
-                                    borderRadius: BorderRadius.circular(10)
-                                ),
-                                child: TextFormField(
-                                  controller: daysController,
-
-                                  decoration: InputDecoration(
-                                    label: Text('days'),
-                                    hintText: "Enter days",
-                                    border:  OutlineInputBorder(borderRadius: BorderRadius.circular(10))
-                                  ),
-                                ),
-                              ),
-
-                              index != 0? IconButton(onPressed: (){
-                          doctorNotifier.removeTextField(index);
-                              }, icon: Icon(Icons.delete)):const SizedBox()
-                            ],
+                          buildTextField(textController, context,
+                              daysController, index, doctorNotifier),
+                          SizedBox(
+                            height: 5,
                           ),
-                          SizedBox(height: 5,),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Row(
-                                children: [
-                                  Transform.scale(
-                                    scale: 1.2,
-                                    child: Checkbox(
-                                        value: morning,
-                                        onChanged: (val){
-                                          setState(() {
-                                            doctorNotifier.morningList[index] = val!;
-                                          });
-                                        }),
-                                  ),
-                                  Transform.scale(
-                                    scale: 1.2,
-                                    child: Checkbox(
-                                        value: afternoon,
-                                        onChanged: (val){
-                                          setState(() {
-                                            doctorNotifier.afternoonList[index] = val!;
-                                          });
-                                        }),
-                                  ),
-                                  Transform.scale(
-                                    scale: 1.2,
-                                    child: Checkbox(
-                                        value: night,
-                                        onChanged: (val){
-                                          setState(() {
-                                            doctorNotifier.nightList[index] = val!;
-                                          });
-                                        }),
-                                  )
-                                ],
-                              ),
-                              Spacer(),
-                              Row(
-                                children: [
-                                  Text('Before meal: '),
-                                  Transform.scale(
-                                    scale: 1.2,
-                                    child: Checkbox(
-                                        value: beforeMeal,
-                                        onChanged: (val){
-                                          setState(() {
-                                            doctorNotifier.beforeList[index] = val!;
-                                          });
-                                        }),
-                                  ),
-
-
-                                ],
-                              ),
-                              Spacer(),
-                              Spacer(),
-                            ],
-                          )
+                          buildCheckBox(morning, doctorNotifier, index,
+                              afternoon, night, beforeMeal)
                         ],
                       ),
                     );
-                  },),
+                  },
                 ),
-                CustomButton(pressed: (){
+              ),
+              CustomButton(
+                pressed: () {
                   // generate prescription
-                }, btnName: "Generate Prescription",backgroundColor: Colors.lime,),
-                SizedBox(height: 10,),
-              ],
-            );
-          },),
-floatingActionButton: FloatingActionButton.extended(onPressed: (){
-_controller.generateTextEditingController();
-_controller.generateCheckBoxRule();
-_controller.generateCheckBoxMeal();
-}, label: Text('Add more'),
-icon: Icon(Icons.add),),
+                },
+                btnName: "Generate Prescription",
+                backgroundColor: Colors.lime,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+            ],
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          _controller.generateTextEditingController();
+          _controller.generateCheckBoxRule();
+          _controller.generateCheckBoxMeal();
+        },
+        label: Text('Add more'),
+        icon: Icon(Icons.add),
+      ),
+    );
+  }
+
+  Row buildTextField(
+      TextEditingController textController,
+      BuildContext context,
+      TextEditingController daysController,
+      int index,
+      DoctorController doctorNotifier) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        CustomTextField(
+            fieldController: textController,
+            label: "Medicine Name",
+            textInputType: TextInputType.text),
+        Container(
+          width: MediaQuery.of(context).size.width * 0.08,
+          decoration: BoxDecoration(
+              color: Colors.white, borderRadius: BorderRadius.circular(10)),
+          child: TextFormField(
+            controller: daysController,
+            decoration: InputDecoration(
+                label: Text('days'),
+                hintText: "Enter days",
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10))),
+          ),
+        ),
+        index != 0
+            ? IconButton(
+                onPressed: () {
+                  doctorNotifier.removeTextField(index);
+                },
+                icon: Icon(Icons.delete))
+            : const SizedBox()
+      ],
+    );
+  }
+
+  Row buildCheckBox(bool morning, DoctorController doctorNotifier, int index,
+      bool afternoon, bool night, bool beforeMeal) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Row(
+          children: [
+            Transform.scale(
+              scale: 1.2,
+              child: Checkbox(
+                  value: morning,
+                  onChanged: (val) {
+                    setState(() {
+                      doctorNotifier.morningList[index] = val!;
+                    });
+                  }),
+            ),
+            Transform.scale(
+              scale: 1.2,
+              child: Checkbox(
+                  value: afternoon,
+                  onChanged: (val) {
+                    setState(() {
+                      doctorNotifier.afternoonList[index] = val!;
+                    });
+                  }),
+            ),
+            Transform.scale(
+              scale: 1.2,
+              child: Checkbox(
+                  value: night,
+                  onChanged: (val) {
+                    setState(() {
+                      doctorNotifier.nightList[index] = val!;
+                    });
+                  }),
+            )
+          ],
+        ),
+        Spacer(),
+        Row(
+          children: [
+            Text('Before meal: '),
+            Transform.scale(
+              scale: 1.2,
+              child: Checkbox(
+                  value: beforeMeal,
+                  onChanged: (val) {
+                    setState(() {
+                      doctorNotifier.beforeList[index] = val!;
+                    });
+                  }),
+            ),
+          ],
+        ),
+        Spacer(),
+        Spacer(),
+      ],
     );
   }
 }
