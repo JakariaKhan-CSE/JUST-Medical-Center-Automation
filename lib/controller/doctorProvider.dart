722131657cb999e82622f8c0c9279f60dd547f82
Md. Jakaria Ibna Azam Khan. <mdjakariaibnaazamkhan@gmail.com>
2024-11-20 12:33:15 +0600

    optimize get doctor list call.Ensure Fetch doctorList  on first build. next time not repeated call again backend.

--- lib/controller/doctorProvider.dart
+++ lib/controller/doctorProvider.dart
@@ -3,9 +3,20 @@ import 'package:just_medical_center_automation/model/res/patient/doctorResponse.
 import 'package:just_medical_center_automation/services/helper/doctorHelper.dart';
 
 class DoctorNotifier extends ChangeNotifier{
-  late Future<List<TotalDoctors>> doctorList;
+  List<TotalDoctors> _doctorList = [];
+  bool _isLoaded = false; // To ensure data is only fetched once
 
-  getDoctors(){
-    doctorList = DoctorHelper.getDoctors(); // getJobs function static tai JobHelper() airokom kora lageni
+  List<TotalDoctors> get doctorList => _doctorList;
+
+  Future<void> getAllDoctorList() async {
+    if (_isLoaded) return; // Prevent re-fetching
+    try {
+      _doctorList =await DoctorHelper.getDoctors(); // be careful to use await otherwise get error
+      _isLoaded = true; // Mark data as loaded
+      notifyListeners();
+    } catch (e) {
+      debugPrint('Error fetching prescriptions: $e');
+    }
   }
+
 }
--- lib/screen/patient/widget/patient/home/doctor_listbuilder.dart
+++ lib/screen/patient/widget/patient/home/doctor_listbuilder.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:just_medical_center_automation/controller/doctorProvider.dart';
-import 'package:just_medical_center_automation/model/res/patient/doctorResponse.dart';
 import 'package:provider/provider.dart';
-
-import '../../../../../data/doctorData.dart';
 
 
 class doctorListbuilder extends StatelessWidget {
@@ -13,80 +10,81 @@ class doctorListbuilder extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Fetch prescriptions on first build
+    final doctorNotifier = Provider.of<DoctorNotifier>(context, listen: false);
+    doctorNotifier.getAllDoctorList();
+
     return Consumer<DoctorNotifier>(
       builder: (context, doctorNotifier, child) {
-        // Firstly getAll doctors
-        doctorNotifier.getDoctors();
-      return Expanded(
-        // if nested class avoid this model. remove useless attribute and make single model. So that easily access each attribute
-        child: FutureBuilder<List<TotalDoctors>>(
-          future: doctorNotifier.doctorList,
-          builder: (context, snapshot) {
-            if (snapshot.connectionState ==
-                ConnectionState.waiting) {
-              return Center(child: CircularProgressIndicator(),);
-            } else if (snapshot.hasError) {
-              return Center(
-                child: Text('Error is: ${snapshot.error}'),
-              );
-            }
-        else{
-              final doctors = snapshot.data; // all doctor
-              return GridView.builder(
-                shrinkWrap: true,
+        // access list
+        final doctors = doctorNotifier.doctorList;
+        if (doctors.isEmpty) {
+          return Center(
+            child: Text(
+              'No Doctors Found',
+              style: Theme.of(context).textTheme.displaySmall,
+            ),
+          );
+        }
 
-                itemCount: doctors?.length,
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,crossAxisSpacing: 2,mainAxisSpacing: 1),
-                itemBuilder: (context, index) {
-                  final doctor = doctors?[index];
-                  return Padding(
-                    padding: const EdgeInsets.all(4.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Expanded(
-                            flex: 6,
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.circular(10),
-                              child: Image.network(
-                                doctor?.profile??'',
-                                fit: BoxFit.contain,
-                                width: 150,
-                                height: 150,
-                              ),
-                            )),
-                        Expanded(
-                            flex: 3,
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                const SizedBox(
-                                  height: 10,
-                                ),
-                                Text(
-                                  doctor!.name!,overflow: TextOverflow.ellipsis,
-                                  style: const TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 15,
-                                      color: Colors.black),
-                                ),
-                                SizedBox(height: 5,),
-                                Text(
-                                  doctor.specialist??'MBBS Doctor',overflow: TextOverflow.ellipsis,
-                                  style: const TextStyle(fontSize: 12, color: Colors.grey),
-                                ),
-                              ],
-                            )),
-                      ],
-                    ),
-                  );
-                },);
-            }
+        return Expanded(
+            // if nested class avoid this model. remove useless attribute and make single model. So that easily access each attribute
+            child: GridView.builder(
+          shrinkWrap: true,
+          itemCount: doctors.length,
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2, crossAxisSpacing: 2, mainAxisSpacing: 1),
+          itemBuilder: (context, index) {
+            final doctor = doctors[index];
+            return Padding(
+              padding: const EdgeInsets.all(4.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                      flex: 6,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(10),
+                        child: Image.network(
+                          doctor.profile ?? '',
+                          fit: BoxFit.contain,
+                          width: 150,
+                          height: 150,
+                        ),
+                      )),
+                  Expanded(
+                      flex: 3,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          Text(
+                            doctor.name!,
+                            overflow: TextOverflow.ellipsis,
+                            style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 15,
+                                color: Colors.black),
+                          ),
+                          const SizedBox(
+                            height: 5,
+                          ),
+                          Text(
+                            doctor.specialist ?? 'MBBS Doctor',
+                            overflow: TextOverflow.ellipsis,
+                            style: const TextStyle(
+                                fontSize: 12, color: Colors.grey),
+                          ),
+                        ],
+                      )),
+                ],
+              ),
+            );
           },
-
-        ),
-      );
-    },);
+        ));
+      },
+    );
   }
 }
