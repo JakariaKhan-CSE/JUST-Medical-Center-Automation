ca718a56e4863a244e02d0f6cbff2f91da4d9d42
Md. Jakaria Ibna Azam Khan <mdjakariaibnaazamkhan@gmail.com>
2025-01-15 21:24:21 +0600

    prescription design error fix.

--- lib/widget/common/prescription_left_side.dart
+++ lib/widget/common/prescription_left_side.dart
@@ -7,39 +7,37 @@ class prescription_left_side extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Expanded(
-        flex: 1,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Cheif Complaints:',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 13),
-            ),
-            SizedBox(
-              height: 10,
-            ),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'Cheif Complaints:',
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 13),
+        ),
+        SizedBox(
+          height: 10,
+        ),
 
-            Text(
-              'improving',
-              style: TextStyle(fontSize: 13),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Divider(),
-            Text(
-              'Diagnosis: ',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 13),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Text(
-              'under evaluation',
-              style: TextStyle(fontSize: 13),
-            ),
-          ],
-        ));
+        Text(
+          'improving',
+          style: TextStyle(fontSize: 13),
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Divider(),
+        Text(
+          'Diagnosis: ',
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 13),
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Text(
+          'under evaluation',
+          style: TextStyle(fontSize: 13),
+        ),
+      ],
+    );
   }
 }
