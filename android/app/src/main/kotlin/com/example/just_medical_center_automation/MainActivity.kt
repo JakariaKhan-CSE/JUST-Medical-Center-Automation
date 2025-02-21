502a57b232673eac4ce58de0091f23132c3f554d
Md. Jakaria Ibna Azam Khan. <mdjakariaibnaazamkhan@gmail.com>
2025-02-21 07:54:25 +0600

    i successfully solve my gradle error problem, decrease one from latest version compile sdk(34 perfectly work)35 not work and gradlew.wrapper.properties use latest version (create new fresh project then see gradle version).

--- android/app/src/main/kotlin/com/example/just_medical_center_automation/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/just_medical_center_automation/MainActivity.kt
@@ -2,4 +2,5 @@ package com.example.just_medical_center_automation
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+}
