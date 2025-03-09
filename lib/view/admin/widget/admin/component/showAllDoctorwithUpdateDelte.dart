44a4733fd70d7567a9ff1a042c990add72b0e842
Md. Jakaria Ibna Azam Khan. <mdjakariaibnaazamkhan@gmail.com>
2025-03-09 21:02:39 +0600

    see all doctor list redesign complete.

--- lib/view/admin/widget/admin/component/showAllDoctorwithUpdateDelte.dart
+++ lib/view/admin/widget/admin/component/showAllDoctorwithUpdateDelte.dart
@@ -4,7 +4,6 @@ import 'package:just_medical_center_automation/widget/common/customButton.dart';
 
 import '../../../../../responsive.dart';
 
-
 class Showalldoctorwithupdatedelte extends StatefulWidget {
   const Showalldoctorwithupdatedelte({super.key});
 
@@ -20,61 +19,113 @@ class _ShowalldoctorwithupdatedelteState
     final Height = MediaQuery.of(context).size.height;
     final Width = MediaQuery.of(context).size.width;
     return Padding(
-      padding: Responsive.isDesktop(context) ? EdgeInsets.symmetric(horizontal: 30,vertical: 20):
-      EdgeInsets.symmetric(horizontal: 10,vertical: 5),
+      padding: Responsive.isDesktop(context)
+          ? EdgeInsets.symmetric(horizontal: 30, vertical: 20)
+          : EdgeInsets.symmetric(horizontal: 10, vertical: 5),
       child: ListView.builder(
         itemCount: DoctorData.data.length,
         itemBuilder: (context, index) {
           final data = DoctorData.data[index];
           return AspectRatio(
-            aspectRatio: Responsive.isDesktop(context) ? 3: 2.5,
+            aspectRatio: Responsive.isDesktop(context) ? 3 : 2.9,
             child: Container(
-              height: Height*0.3,
+              height: Height * 0.3,
               width: double.infinity,
-              margin: EdgeInsets.symmetric(horizontal: 10,vertical: 20),
+              margin: EdgeInsets.symmetric(horizontal: 10, vertical: 20),
               decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  color: Colors.white
+                  borderRadius: BorderRadius.circular(10), color: Colors.white
                   // color: Colors.grey.withOpacity(0.4)
-              ),
+                  ),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   Row(
                     children: [
-            Image.asset(data['img'],fit: BoxFit.cover,height:Responsive.isDesktop(context) ? Height*0.3:55,width: Responsive.isDesktop(context) ? Width*0.15:55,),
-                      Responsive.isDesktop(context) ? SizedBox(width: 20,):SizedBox(width: 5,),
+                      Padding(
+                        padding: const EdgeInsets.all(4.0),
+                        child: Image.asset(
+                          data['img'],
+                          fit: BoxFit.cover,
+                          height:
+                              Responsive.isDesktop(context) ? Height * 0.3 : 55,
+                          width:
+                              Responsive.isDesktop(context) ? Width * 0.15 : 55,
+                        ),
+                      ),
+                      Responsive.isDesktop(context)
+                          ? SizedBox(
+                              width: 20,
+                            )
+                          : SizedBox(
+                              width: 5,
+                            ),
                       Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                          Text(data['name'],style: Responsive.isDesktop(context) ?Theme.of(context).textTheme.titleLarge!.copyWith(
-                            color: Colors.black
-                          ):Theme.of(context).textTheme.titleMedium!.copyWith(
-                              color: Colors.black
-                          ),),
-                          SizedBox(height: 10,),
-                          Text(data['department'],style: Theme.of(context).textTheme.titleSmall!.copyWith(
-                            color: Colors.blueGrey.withOpacity(0.8)
-                          ),),
+                          Text(
+                            data['name'],
+                            style: Responsive.isDesktop(context)
+                                ? Theme.of(context)
+                                    .textTheme
+                                    .titleLarge!
+                                    .copyWith(color: Colors.black)
+                                : Theme.of(context)
+                                    .textTheme
+                                    .titleMedium!
+                                    .copyWith(color: Colors.black),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Text(
+                            data['department'],
+                            style: Theme.of(context)
+                                .textTheme
+                                .titleSmall!
+                                .copyWith(
+                                    color: Colors.blueGrey.withOpacity(0.8)),
+                          ),
                         ],
                       )
                     ],
                   ),
                   Padding(
                     padding: const EdgeInsets.only(right: 10),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        CustomButton(pressed: (){
-                          //write update database code
-                        }, btnName: 'Update'),
-                        Responsive.isDesktop(context) ? SizedBox(height: 20,):SizedBox(height: 5,),
-                        CustomButton(pressed: (){
-                          //write delete database code
-                        }, btnName: 'Delete'),
-                      ],
+                    child: FittedBox(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          if (Responsive.isMobile(context))
+                            IconButton(onPressed: () {
+                              // doctor edit function here
+                            }, icon: Icon(Icons.edit_calendar,size: 22,color: Colors.orangeAccent,)),
+                          if (Responsive.isMobile(context))
+                            IconButton(onPressed: () {
+                              // doctor delete
+                            }, icon: Icon(Icons.delete, color: Colors.deepOrange,size: 22,)),
+                          if (Responsive.isDesktop(context))
+                            CustomButton(
+                                pressed: () {
+                                  //write update database code
+                                },
+                                btnName: 'Update'),
+                          Responsive.isDesktop(context)
+                              ? SizedBox(
+                                  height: 20,
+                                )
+                              : SizedBox(
+                                  height: 5,
+                                ),
+                          if (Responsive.isDesktop(context))
+                            CustomButton(
+                                pressed: () {
+                                  //write delete database code
+                                },
+                                btnName: 'Delete'),
+                        ],
+                      ),
                     ),
                   )
                 ],
